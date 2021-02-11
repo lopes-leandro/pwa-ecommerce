0e0188938bdfa861ef8440ec08fc45c536eb1a26
Leandro Lopes <lopesleandro.it@gmail.com>
2021-02-11 00:41:09 -0300

    notification change pwa

--- eletronic-store/src/app/app.component.ts
+++ eletronic-store/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProductsService } from './products.service';
-import { CartService } from './cart.service';
-import { SpinnerService } from './spinner/spinner.service';
-import { MatDialog } from '@angular/material/dialog';
 import { CartComponent } from './cart/cart.component';
-
+import { ProductsService } from './products.service';
+import { SpinnerService } from './spinner/spinner.service';
+import { CartService } from './cart.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'es-root',
@@ -22,11 +23,22 @@ export class AppComponent implements OnInit {
     private productsService: ProductsService,
     private cartService: CartService,
     private dialog: MatDialog,
-    private spinnerService: SpinnerService ) {
+    private spinnerService: SpinnerService,
+    update: SwUpdate,
+    private snackBar: MatSnackBar) {
     this.spinnerService.setSpinnerEnable(true);
     this.products$ = this.productsService.getProducts();
     this.cart$ = this.cartService.cart$.subscribe(cart => this.cart = cart);
-    this.spinnerService.setSpinnerEnable(false);    
+    this.spinnerService.setSpinnerEnable(false);
+    
+    update.available.subscribe(event => {
+      this.snackBar.open('Nova atualização disponível', 'Instale agora', {
+        duration: 4000
+      }).onAction().subscribe(() => {
+        update.activateUpdate().then(() => location.reload());
+      });
+    });
+    update.checkForUpdate();
   }
 
   onAddProduct(count, product) {
